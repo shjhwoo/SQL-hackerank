596e750f530532b431300be4eac6e03b7c0829f0
shjhwoo <shjhwoo@trustnhope.com>
2023-05-02 22:44:05 +0900

    서브쿼리 사용해서 해결하기

--- ollivandersInventory.sql
+++ ollivandersInventory.sql
@@ -8,3 +8,21 @@ where wp.is_evil = 0
 order by temp.power desc, wp.age desc
 
 //이것도 안됨
+
+
+-- 다시
+
+SELECT W.id, P.age, W.coins_needed, W.power
+FROM Wands W
+JOIN Wands_property P ON W.Code = P.Code
+WHERE P.is_evil = 0
+AND W.coins_needed = 
+(
+    SELECT MIN(W1.coins_needed) 
+    FROM Wands W1
+    JOIN Wands_property P1 ON W1.Code = P1.Code
+    WHERE P1.is_evil = 0
+    AND W1.power = W.power
+    AND P1.age = P.age    
+)
+ORDER BY W.power DESC, P.age DESC
